d6d0f29968557ae94152aac073337039f00a4bdd
edwardtay <edwardtay7@gmail.com>
2026-03-30 02:39:04 +0800

    feat: React frontend with ElevenLabs ConversationProvider and light theme

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ConversationProvider } from '@elevenlabs/react';
 import { App } from './App';
 import './index.css';
 
+const AGENT_ID = 'agent_5201kmthvevqex5s4qps314ds4s3';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConversationProvider agentId={AGENT_ID}>
+      <App />
+    </ConversationProvider>
   </StrictMode>
 );
